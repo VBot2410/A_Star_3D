d5a2d7d4a6ebf62d7c11abbf8b9e4a498fcd70ac
VBot2410 <vaibhav24.vb@gmail.com>
2017-10-06 05:18:39 -0400

    Completed Implementation of Build_Map class. Task_ID:Dev_2_04

--- include/Build_Map.h
+++ include/Build_Map.h
@@ -1,6 +1,7 @@
 #ifndef INCLUDE_BUILD_MAP_H_
 #define INCLUDE_BUILD_MAP_H_
 
+//#include <vector>
 #include <functional>
 #include <set>
 #include "Planner.h"
@@ -9,6 +10,10 @@
 class Build_Map {
  public:
   Build_Map(std::vector<double>,double,double,double);
+  double x0,y0,z0,x1,y1,z1;
+  std::vector<double>Boundary;
+  double xy_res,z_res,margin;
+  int World_X, World_Y, World_Z;
   std::vector<int>World_Dimensions();
   std::vector<int>Build_Obstacle(std::vector<double>);
   std::vector<int>Build_Node(std::vector<double>);
